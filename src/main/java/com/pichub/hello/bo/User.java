d300e838e6596ce82bf1d9fbaa609dd1638c2706
Yayooooooo <314839179@qq.com>
2019-03-16 15:12:43 +0800

    jyt更新了src.main.java.com.pichub.hello.bo.User

--- src/main/java/com/pichub/hello/bo/User.java
+++ src/main/java/com/pichub/hello/bo/User.java
@@ -1,19 +1,109 @@
 package com.pichub.hello.bo;
 
+import java.util.Date;
+
 public class User {
-    private  Long userId;
-    private  String   userName;
-  /*  email
-            password
-    create_time
-            gender
-    subscription
-            avatar_path
-    capacity
-            used_capacity
-    qq_num
-            wechat_num
-    phone*/
+    private Long userId;
+    private String userName;
+    private String email;
+    private String password;
+    private Date create_time;
+    private char gender;
+    private String subscription;
+    private String avatar_path;
+    private Long capacity;
+    private Long used_capacity;
+    private String wechat_num;
+    private String qq_num;
+    private String phone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getSubscription() {
+        return subscription;
+    }
+
+    public void setSubscription(String subscription) {
+        this.subscription = subscription;
+    }
+
+    public String getAvatar_path() {
+        return avatar_path;
+    }
+
+    public void setAvatar_path(String avatar_path) {
+        this.avatar_path = avatar_path;
+    }
+
+    public Long getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Long capacity) {
+        this.capacity = capacity;
+    }
+
+    public Long getUsed_capacity() {
+        return used_capacity;
+    }
+
+    public void setUsed_capacity(Long used_capacity) {
+        this.used_capacity = used_capacity;
+    }
+
+    public String getWechat_num() {
+        return wechat_num;
+    }
+
+    public void setWechat_num(String wechat_num) {
+        this.wechat_num = wechat_num;
+    }
+
+    public String getQq_num() {
+        return qq_num;
+    }
+
+    public void setQq_num(String qq_num) {
+        this.qq_num = qq_num;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 
     public Long getUserId() {
         return userId;
