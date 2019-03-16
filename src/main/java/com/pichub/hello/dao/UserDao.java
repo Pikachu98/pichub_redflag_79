08b9b4af99cce58d7acb384f0ac0e542cb222ba9
Pikachu98 <326200153@qq.com>
2019-03-16 14:11:47 +0800

    2019.3.16sdfafsadfsde

---
+++ src/main/java/com/pichub/hello/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.pichub.hello.dao;
+
+import com.pichub.hello.bo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserDao {
+    boolean insertUser(User user);
+    boolean existsByPhone(String phone);
+    boolean existsByEmail(String email);
+}
