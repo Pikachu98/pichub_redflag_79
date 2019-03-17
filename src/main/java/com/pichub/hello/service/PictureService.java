7cbd39b95e28dbd541e372d07bf925ac0956e2b2
CatsAndDoge <hw_stack@163.com>
2019-03-17 13:06:18 +0800

    2019.3.17

--- src/main/java/com/pichub/hello/service/PictureService.java
+++ src/main/java/com/pichub/hello/service/PictureService.java
@@ -1,7 +1,10 @@
 package com.pichub.hello.service;
 
+import com.pichub.hello.bo.Picture;
+
 /**
  * Created by root on 19-3-16.
  */
 public interface PictureService {
+    Picture getPicture(long pictureId) throws Exception;
 }
--- src/main/java/com/pichub/hello/web/PictureController.java
+++ src/main/java/com/pichub/hello/web/PictureController.java
@@ -1,7 +1,30 @@
 package com.pichub.hello.web;
 
+import com.pichub.hello.bo.Picture;
+import com.pichub.hello.service.PictureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Created by root on 19-3-16.
  */
+@Controller
 public class PictureController {
+    @Autowired
+    PictureService pictureService;
+
+    @RequestMapping(value = "/picture/{pictureId}")
+    public String pictureDetail(@PathVariable long pictureId, ModelMap model,
+                                HttpServletRequest request, HttpServletResponse response)throws Exception{
+        Picture picture = pictureService.getPicture(pictureId);
+        model.put("p", picture);
+
+        return "picture";
+    }
 }
