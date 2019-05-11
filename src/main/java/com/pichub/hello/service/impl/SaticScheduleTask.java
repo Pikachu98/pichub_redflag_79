2bebecbae667995440363df462975d08d67e11de
shunhuayang <1217326265@qq.com>
2019-05-11 14:12:08 +0800

    tag-ysh

--- src/main/java/com/pichub/hello/service/impl/SaticScheduleTask.java
+++ src/main/java/com/pichub/hello/service/impl/SaticScheduleTask.java
@@ -1,13 +1,11 @@
 package com.pichub.hello.service.impl;
 
-import com.pichub.hello.service.impl.DoTagService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 
 @Component
 @Configuration      //1.主要用于标记配置类，兼备Component的效果。
