e334dad46822a87c98095691e8d563b7746fe4e7
Asukiran <1531881520@qq.com>
2019-05-11 11:47:20 +0800

    修改图片路径

--- src/main/java/com/pichub/hello/web/AlbumController.java
+++ src/main/java/com/pichub/hello/web/AlbumController.java
@@ -64,6 +64,30 @@ public class AlbumController {
         model.put("FocusMe",focusService.showFocusMe(User.getCurrentUser(request).getUserId().intValue()).size());
         model.put("albumList",myAlbumList);
         listAlbum(model,request);
+
+        String p = request.getParameter("page");
+        int page;
+        try{
+            page = Integer.valueOf(p);
+        }catch (NumberFormatException e){
+            page=1;
+        }
+        int totalAlbum = myAlbumList.size();
+        int albumPerPage = 10;
+        int totalPages = totalAlbum%albumPerPage==0?totalAlbum/albumPerPage:totalAlbum/albumPerPage+1;
+        int beginIndex = (page-1)*albumPerPage;
+        int endIndex = beginIndex+albumPerPage;
+        if(endIndex>totalPages)
+            endIndex = totalPages;
+
+        request.setAttribute("totalAlbum",totalAlbum);
+        request.setAttribute("albumPerPage",albumPerPage);
+        request.setAttribute("totalPages",totalPages);
+        request.setAttribute("beginIndex",beginIndex);
+        request.setAttribute("endIndex",endIndex);
+        request.setAttribute("page",page);
+        request.setAttribute("albumList",myAlbumList);
+
         return "myalbum";
 
     }
@@ -115,15 +139,12 @@ public class AlbumController {
     public void deleteAlbum(long albumId, HttpServletResponse response, HttpServletRequest request)throws Exception
     {
         albumService.deleteAlbum(albumId);
-
-        response.sendRedirect("myalbum");
     }
 
     @RequestMapping("/changeAlbumName")
     public void changeName(long albumId, String name, HttpServletRequest request, HttpServletResponse response)throws Exception
     {
         albumService.changeName(albumId, name);
-        response.sendRedirect("myalbum");
     }
 
 }
